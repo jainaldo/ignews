e403520c3850a2c0383958f3a82e06675657ed36
jainaldo <jainaldosilva@gmail.com>
2023-03-30 20:56:39 -0300

    Title dinâmico por página

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,6 @@ import Head from 'next/head';
 
 export default function App({ Component, pageProps } : AppProps) {
   return (
-    <>
-      <Head>
-        <title>ig.news</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <Component {...pageProps} />
   )
 }
