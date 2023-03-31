ff06888729a7823c14af472f964675bbeebfbbc9
jainaldo <jainaldosilva@gmail.com>
2023-03-30 21:37:59 -0300

    Compenente: Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from "next/app";
 import "../styles/global.scss";
+import { Header } from '../components/Header/index';
 
 export default function App({ Component, pageProps } : AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
   )
 }
