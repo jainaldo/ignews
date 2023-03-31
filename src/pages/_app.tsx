4691fe10b3b1912ccae6e8b17ec6d85bd1713115
jainaldo <jainaldosilva@gmail.com>
2023-03-30 21:08:49 -0300

    Estilos globais do app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import Head from 'next/head';
+import "../styles/global.scss";
 
 export default function App({ Component, pageProps } : AppProps) {
   return (
