9cb5e670fa8cb57295426b90534d4eb34bfc433b
jainaldo <jainaldosilva@gmail.com>
2023-03-31 21:21:20 -0300

    Authenticação com NextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from "next/app";
 import "../styles/global.scss";
 import { Header } from '../components/Header/index';
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps } : AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
-  )
+    </SessionProvider>
+  );
 }
