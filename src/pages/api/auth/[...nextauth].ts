c6735b415e4116a4801644d959a8e13acef278b1
jainaldo <jainaldosilva@gmail.com>
2023-04-01 12:24:35 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb';
+import { fauna } from "../../../services/fauna";
+
 
 export const authOptions = {
   providers: [
@@ -8,6 +11,22 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
+  }
 }
 
 export default NextAuth(authOptions)
