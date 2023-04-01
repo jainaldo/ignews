242ea496e8426b1bc7f6e130bc9847a3245903a4
jainaldo <jainaldosilva@gmail.com>
2023-04-01 18:47:24 -0300

    Verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,11 +16,16 @@ export const authOptions = {
       const { email } = user;
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
           )
-        )
+        );
         return true;
       } catch (error) {
         return false;
