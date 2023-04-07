5a1a439a4789e5a7ccc52228717dc3415a12b7b7
jainaldo <jainaldosilva@gmail.com>
2023-04-07 17:15:59 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,21 @@
 import Head from "next/head";
 import styles from './styles.module.scss'
 import { GetStaticProps } from "next";
+import * as PrismicHelper from '@prismicio/helpers';
 import { getPrismicClient } from "../../services/prismic";
-import { stringify } from "querystring";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,26 +24,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Axios - um cliente HTTP Full Stack</strong>
-            <p>Axios é um cliente HTTP baseado em Promises para fazer requisições. Pode ser utilizado tanto no navegador quando no Node.js</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Axios - um cliente HTTP Full Stack</strong>
-            <p>Axios é um cliente HTTP baseado em Promises para fazer requisições. Pode ser utilizado tanto no navegador quando no Node.js</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Axios - um cliente HTTP Full Stack</strong>
-            <p>Axios é um cliente HTTP baseado em Promises para fazer requisições. Pode ser utilizado tanto no navegador quando no Node.js</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Axios - um cliente HTTP Full Stack</strong>
-            <p>Axios é um cliente HTTP baseado em Promises para fazer requisições. Pode ser utilizado tanto no navegador quando no Node.js</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -42,12 +40,23 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const posts = await prismic.getByType("post", {
+  const response = await prismic.getByType("post", {
     fetch: ['post.title', 'post.content'],
     pageSize: 100
   })
 
-  console.log(JSON.stringify(posts, null, 2));
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: PrismicHelper.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
     props: {
